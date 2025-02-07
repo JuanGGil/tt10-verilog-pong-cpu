d452f754b5d84cf6f4d2c63a4b56c4b4ed4667d5
JuanGGil <jgg8979@nyu.edu>
2025-02-07 13:28:57 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,12 +10,28 @@ module tt_um_PongGame (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+
+    //----- Since this is just the pong portion of the entire TTO design, these should be our IO instead
+    //input wire [2:0] ui_in,
+    //input wire clk,
+    //output wire [9:0] paddle_y
+    //output wire [9:0] opponent_paddle_y
+    //output wire [9:0] current_ball_x
+    //output wire [9:0] current_ball_y
+    //output wire [8:0] score (top half opponent score, bottom half player score)
+
+
+    
+
+    
 );
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, rst_n, 1'b0};
 
     // Parameters
+    // these might be better to import into the module instead of instantiating this here (for more variable control)
+    
     parameter SCREEN_WIDTH = 640;
     parameter SCREEN_HEIGHT = 480;
     parameter BALL_SIZE = 10;
