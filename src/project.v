a9076f67a3db5d1c64b5b237419fc5ce70afa29b
JuanGGil <jgg8979@nyu.edu>
2025-02-21 13:41:39 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -45,7 +45,7 @@ module tt_um_PongGame (
 );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, rst_n, 1'b0};
+    wire _unused = &{ena, rst_n, uio_in, 1'b0};
     
 
     // Parameters
