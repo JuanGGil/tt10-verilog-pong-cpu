8103c45afd167a31eeb093b4cde452c0e456faca
JuanGGil <jgg8979@nyu.edu>
2025-02-25 11:54:37 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -42,37 +42,42 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     f = open('test_vga_output.txt', 'w').close()
-    f = open("test_vga_output.txt", "a")
 
-    for j in range(5):
-        for i in range(2*(800*525)+10): # 30 clock cycles
-        #for i in range(10): # 30 clock cycles
-    
-            
-            
-            # assert pow(2,ball_dir_x)+ball_dir_y == dut.uo_out
-            #await ClockCycles(dut.clk, 65540)
-            await ClockCycles(dut.clk, 1)
-    
-            time = i * 40 + j * (((2*800*525)+10)*40)
-            
-            hsync = dut.uo_out[7].value
-            
-            vsync = dut.uo_out[6].value
-            
-            red_1 = dut.uo_out[5].value
-            red_0 = dut.uo_out[4].value
-            
-            blue_1 = dut.uo_out[3].value
-            blue_0 = dut.uo_out[2].value
-            
-            green_1 = dut.uo_out[1].value
-            green_0 = dut.uo_out[0].value
-            
-    
-            #dut._log.info(f"{time} ns: {hsync} {vsync} 0{red_1}{red_0} 0{blue_1}{blue_0} {green_1}{green_0}\n")
-            f.write(f"{time} ns: {hsync} {vsync} 0{red_1}{red_0} 0{blue_1}{blue_0} {green_1}{green_0}\n")
-        await ClockCycles(dut.clk, 800*525*60)
+    await ClockCycles(dut.clk, 1000000)
+    dut._log.info("waiting finished")
     f.close()
+    
+    # f = open("test_vga_output.txt", "a")
+    
+    # for j in range(5):
+    #     for i in range(2*(800*525)+10): # 30 clock cycles
+    #     #for i in range(10): # 30 clock cycles
+    
+            
+            
+    #         # assert pow(2,ball_dir_x)+ball_dir_y == dut.uo_out
+    #         #await ClockCycles(dut.clk, 65540)
+    #         await ClockCycles(dut.clk, 1)
+    
+    #         time = i * 40 + j * (((2*800*525)+10)*40)
+            
+    #         hsync = dut.uo_out[7].value
+            
+    #         vsync = dut.uo_out[6].value
+            
+    #         red_1 = dut.uo_out[5].value
+    #         red_0 = dut.uo_out[4].value
+            
+    #         blue_1 = dut.uo_out[3].value
+    #         blue_0 = dut.uo_out[2].value
+            
+    #         green_1 = dut.uo_out[1].value
+    #         green_0 = dut.uo_out[0].value
+            
+    
+    #         #dut._log.info(f"{time} ns: {hsync} {vsync} 0{red_1}{red_0} 0{blue_1}{blue_0} {green_1}{green_0}\n")
+    #         f.write(f"{time} ns: {hsync} {vsync} 0{red_1}{red_0} 0{blue_1}{blue_0} {green_1}{green_0}\n")
+    #     await ClockCycles(dut.clk, 800*525*60)
+    # f.close()
 
 
